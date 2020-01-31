b145077cf1f1b30e7a67efdf5e4f60f6dcabb12c
kunal kumar barman <kumar96kunal@gmail.com>
2020-01-31 09:20:57 -0800

    Add files via upload

--- fire_set.py
+++ fire_set.py
@@ -1,15 +1,5 @@
-'''
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-analytics.js"></script>
-
-<script>
-'''
-''' 
-var firebaseConfig = {
+import pyrebase
+Config = {
     "apiKey": "AIzaSyAqGk4Z_2QAE0JSyvVNDkjhRUmrg1apot0",
     "authDomain": "vsit-2be9f.firebaseapp.com",
     "databaseURL": "https://vsit-2be9f.firebaseio.com",
@@ -19,17 +9,38 @@ var firebaseConfig = {
     "appId": "1:443551929217:web:83d9e1fe6429f56b5c4e80",
     "measurementId": "G-2M0SJERF9S"
 }
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
+
 
 firbase = pyrebase.initialize_app(Config)
 db = firbase.database()
-app = Flask(__name__)
-db.("place").remove()
-name = db.child("place").get()
-place = name.val()
-places = place.values()
-'''
+
+#db.("place").remove()
+dict1={}
+Businesses = db.child("VSIT_files").get()
+for user in Businesses.each():
+  userid = user.key()
+  dict1 = user.val().copy()
+  #print(user.val())
+  inventorydb = db.child(userid).get()
+list_record=[]
+
+
+for i in dict1.values():
+  if i!="Record":
+    list_record.append(i)
+  
+list_value=[]
+for i in range(len(list_record)):
+  if i==0 or i==1 or i==2:
+    continue
+  else:
+    list_value.append(list_record[i]) 
+print(list_value)    
+
+  
+#db.child("VSIT_files").remove()
+#name.child("Rishav").get()
+#paitent = name.val()
+#print(paitent.values())
+#places = place.values()
+
